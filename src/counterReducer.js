f233b0a6592c1545b30f382148a38d576a6413ca
Tushar Upadhyay <113006456+tushar-upadhya@users.noreply.github.com>
2022-12-29 11:57:57 +0530

    Create counterReducer.js

---
+++ src/counterReducer.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+
+
+const counterReducer = (state,action) =>{
+    console.log(state, action);
+    if (action.type === "INCREMENT") {
+        return state + 1; 
+    }
+    if (action.type === "DECREMENT") {
+        return state - 1; 
+    }
+    return state;
+}
+
+export  {counterReducer}
